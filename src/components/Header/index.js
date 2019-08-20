2b3d46e75ee40483e24e0a2cce69ac53b606c45c
Gabriel Batista <svbgabriel@gmail.com>
2019-08-19 21:24:32 -0300

    Implementa o Header dinâmico

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
+import { useSelector, useDispatch } from 'react-redux';
+import { signOut } from '~/store/modules/auth/actions';
+import logo from '~/assets/logo.svg';
 import { Container, ProfileContainer, TextContainer, Profile } from './styles';
 
 export default function Header() {
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
   return (
     <Container>
       <img src={logo} alt="logo" />
       <ProfileContainer>
         <TextContainer>
-          <p>Gabriel Batista</p>
+          <p>{profile.name}</p>
           <Profile to="/profile">Meu Perfil</Profile>
         </TextContainer>
-        <button type="button">Sair</button>
+        <button onClick={handleSignOut} type="button">
+          Sair
+        </button>
       </ProfileContainer>
     </Container>
   );
